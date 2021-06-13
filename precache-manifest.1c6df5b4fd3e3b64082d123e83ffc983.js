96d133ac24af3cdf98c1f558b794d93bd8cb16f9
Ayush Jain <ayush.ayushjain12@gmail.com>
2021-06-14 00:58:38 +0530

    Updates

---
+++ precache-manifest.1c6df5b4fd3e3b64082d123e83ffc983.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4d2f7e9e0df7c5cb7103da82888cfe",
+    "url": "/.github.io/Tea_Co./index.html"
+  },
+  {
+    "revision": "6424564cacd13b9af8a8",
+    "url": "/.github.io/Tea_Co./static/css/3.88ff2523.chunk.css"
+  },
+  {
+    "revision": "e285d30df6c756ef8d6a",
+    "url": "/.github.io/Tea_Co./static/css/4.8c4847df.chunk.css"
+  },
+  {
+    "revision": "f80a1392cb7dd8a8800a",
+    "url": "/.github.io/Tea_Co./static/css/main.dcc742c3.chunk.css"
+  },
+  {
+    "revision": "71508e768ee0e4f186d6",
+    "url": "/.github.io/Tea_Co./static/js/2.b244f337.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/.github.io/Tea_Co./static/js/2.b244f337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6424564cacd13b9af8a8",
+    "url": "/.github.io/Tea_Co./static/js/3.0a1f3c18.chunk.js"
+  },
+  {
+    "revision": "e285d30df6c756ef8d6a",
+    "url": "/.github.io/Tea_Co./static/js/4.e196ec75.chunk.js"
+  },
+  {
+    "revision": "f80a1392cb7dd8a8800a",
+    "url": "/.github.io/Tea_Co./static/js/main.26ad3d35.chunk.js"
+  },
+  {
+    "revision": "48b1e21a120f4d0d4e16",
+    "url": "/.github.io/Tea_Co./static/js/runtime-main.9f90ca98.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/.github.io/Tea_Co./static/media/4.4 crown.svg.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/.github.io/Tea_Co./static/media/shopping-bag.30f07071.svg"
+  }
+]);
