a9e30f2c291b5684f49fdbcd7154c64070d5fdec
Ayush Jain <ayush.ayushjain12@gmail.com>
2021-06-14 01:14:04 +0530

    a

--- public/js/custom.js
+++ public/js/custom.js
@@ -6,22 +6,7 @@ $(function () {
 
 	"use strict";
 
-	/* Header slideshow
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	let myIndex = 0;
-	carousel();
 
-  function carousel() {
-    var i;
-    var x = document.getElementsByClassName("slide_img");
-  	for (i = 0; i < x.length; i++) {
-    		x[i].style.display = "none";  
-  		}
-  	myIndex++;
-  	if (myIndex > x.length) {myIndex = 1}    
-  	x[myIndex-1].style.display = "block";  
-  	setTimeout(carousel, 2000); // Change image every 2 seconds
-}
 
 	/* Preloader
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
